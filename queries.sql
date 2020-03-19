031c338e9161fadf08e27750ed40279f188292bb
dpicht <dpicht@gmail.com>
2020-03-18 20:36:20 -0500

    Finished Assignment

--- queries.sql
+++ queries.sql
@@ -2,7 +2,16 @@
 SELECT * FROM job;
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.job_id IS NOT NULL
+ORDER BY name;
